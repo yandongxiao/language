d9089ad0d1b171b21a9d3647278c6b7eb72c237d
yandongxiao <yandongxiao@chinac.com>
2017-06-02 11:18:35 +0800

    transforming_and_reducing_data_at_the_same_time

--- python-cookbook-master/src/1/transforming_and_reducing_data_at_the_same_time/example.py
+++ python-cookbook-master/src/1/transforming_and_reducing_data_at_the_same_time/example.py
@@ -1,8 +1,11 @@
 #! /usr/bin/python
+# encoding: utf-8
+
 # example.py
 #
 # Some examples of using generators in arguments
 
+# 当生成器表达式作为一个单独参数传递给函数时候的巧妙语法(你并不需要多加一个括号)
 import os
 files = os.listdir(os.path.expanduser('~'))
 if any(name.endswith('.py') for name in files):
@@ -21,5 +24,9 @@ portfolio = [
    {'name':'AOL', 'shares': 20},
    {'name':'SCOX', 'shares': 65}
 ]
+
 min_shares = min(s['shares'] for s in portfolio)
-print(min_shares)
+print(min_shares)   # 只会输出20
+
+min_shares = min(portfolio, key=lambda s: s['shares'])
+print(min_shares)   # 输出列表中一个完整的元素
