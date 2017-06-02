c3719b39fc965a192c3d2dec603bdec5e5410c25
yandongxiao <yandongxiao@chinac.com>
2017-06-02 14:53:18 +0800

    dict的key和item方法返回的视图对象支持集合操作

--- python-cookbook-master/src/1/finding_out_what_two_dictionaries_have_in_common/example.py
+++ python-cookbook-master/src/1/finding_out_what_two_dictionaries_have_in_common/example.py
@@ -15,6 +15,9 @@ b = {
    'y' : 2
 }
 
+# 字典的 keys() 方法返回一个展现键集合的键视图对象。 键视图的一个很少被了解的特性就是它们也支持集合操作
+# 可以直接使用键视图对象而不用先将它们转换成一个set
+# 字典的 items() 方法返回的视图对象也支持集合操作
 print('Common keys:', a.viewkeys() & b.viewkeys())
 print('Keys in a not in b:', a.viewkeys() - b.viewkeys())
 #print('(key,value) pairs in common:', a.items() & b.items())
