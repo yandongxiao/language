5d5fc7c053809d286dd4e0825bfc0543043ba174
yandongxiao <yandongxiao@chinac.com>
2017-06-02 11:51:03 +0800

    mapping_names_to_sequence_elements

--- python-cookbook-master/src/1/mapping_names_to_sequence_elements/example1.py
+++ python-cookbook-master/src/1/mapping_names_to_sequence_elements/example1.py
@@ -1,14 +1,22 @@
 #! /usr/bin/python
-# example.py
+# encoding: utf-8
+
+# 命名元组的核心作用是解决以下表区分不同对象所带来的问题
 
 from collections import namedtuple
-
+# namedtuple函数返回Python中标准元组类型子类的一个工厂方法
 Stock = namedtuple('Stock', ['name', 'shares', 'price'])
 
 def compute_cost(records):
     total = 0.0
     for rec in records:
+        # 实例化
         s = Stock(*rec)     # the parameter is a tuple.
+        # 尽管它看起来像是一个类实例，但是它跟元组类型是可交换的
+        # 不像字典那样，一个命名元组是不可更改的
+        # 更改方法如下s = s._replace(price=75)
+        # _, shares, price = s
+        # total += shares * price
         total += s.shares * s.price
     return total
 
@@ -20,4 +28,3 @@ records = [
 ]
 
 print(compute_cost(records))
-
