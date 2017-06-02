50931de86b71f6ec5171fb272a4a5ea95d80fb96
yandongxiao <yandongxiao@chinac.com>
2017-06-02 14:42:43 +0800

    leaning notes

--- python-cookbook-master/src/1/notes/itemgetter2.py
+++ python-cookbook-master/src/1/notes/itemgetter2.py
@@ -2,17 +2,19 @@
 # encoding: utf-8
 
 persons = [
-    {"first name": "alice", "second name": "roony"},
-    {"first name": "alice", "second name": "jose"},
+    {"first name": "alice", "second": "roony"},
+    {"first name": "alice", "second": "jose"},
 ]
 
 
-for item in sorted(persons, key=lambda p: p["second name"]):
+for item in sorted(persons, key=lambda p: p["second"]):
     print item
 
 # attrgetter中包含的字符串需要是对象的一个属性
 # attrgetter可以按照多个属性进行排序
 # 所以attrgetter对于类实例来说很好用
-#from operator import attrgetter
-#for item in sorted(persons, key=attrgetter("second name")):
-#    print item
+# NOTE: 此处应该使用itemgetter
+from operator import attrgetter
+from operator import itemgetter
+for item in sorted(persons, key=itemgetter("first name", "second")):
+    print item
