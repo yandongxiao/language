34209b39a86c04c65d6f162f9ecf23b22c6478ba
yandongxiao <yandongxiao@chinac.com>
2017-06-02 15:00:40 +0800

    learning removing_duplicates_from_a_sequence_while_maintaining_order

--- python-cookbook-master/src/1/removing_duplicates_from_a_sequence_while_maintaining_order/example2.py
+++ python-cookbook-master/src/1/removing_duplicates_from_a_sequence_while_maintaining_order/example2.py
@@ -1,24 +1,26 @@
 #! /usr/bin/python
+# encoding: utf-8
+
 # example2.py
 #
 # Remove duplicate entries from a sequence while keeping order
 
 def dedupe(items, key=None):
     seen = set()
-    for item in items:
-        val = item if key is None else key(item)
+    for item in items:  # iterable object
+        # 注意if/else的应用
+        val = item if key is None else key(item)    # 获取hashable元素
         if val not in seen:
             yield item
             seen.add(val)
 
 if __name__ == '__main__':
-    a = [ 
+    a = [
         {'x': 2, 'y': 3},
         {'x': 1, 'y': 4},
         {'x': 2, 'y': 3},
         {'x': 2, 'y': 3},
         {'x': 10, 'y': 15}
         ]
-    print(a)
-    print(list(dedupe(a, key=lambda a: (a['x'],a['y']))))
-
+    print a
+    print list(dedupe(a, key=lambda a: (a['x'], a['y'])))
