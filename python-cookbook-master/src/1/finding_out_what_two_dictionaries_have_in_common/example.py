3c45d3799228b1a2f5245fbea9c8c98b01ae6154
yandongxiao <yandongxiao@chinac.com>
2017-06-05 17:32:55 +0800

    finding_out_what_two_dictionaries_have_in_common-2

--- python-cookbook-master/src/1/finding_out_what_two_dictionaries_have_in_common/example.py
+++ python-cookbook-master/src/1/finding_out_what_two_dictionaries_have_in_common/example.py
@@ -1,4 +1,6 @@
 #! /usr/bin/python
+# encoding: utf-8
+
 # example.py
 #
 # Find out what two dictionaries have in common
@@ -18,10 +20,6 @@ b = {
 # 字典的 keys() 方法返回一个展现键集合的键视图对象。 键视图的一个很少被了解的特性就是它们也支持集合操作
 # 可以直接使用键视图对象而不用先将它们转换成一个set
 # 字典的 items() 方法返回的视图对象也支持集合操作
-print('Common keys:', a.viewkeys() & b.viewkeys())
-print('Keys in a not in b:', a.viewkeys() - b.viewkeys())
-#print('(key,value) pairs in common:', a.items() & b.items())
-
-for key in a.viewkeys() & b.viewkeys():
-    if a[key] == b[key]:
-        print (key, a[key])
+print('Common keys:', a.keys() & b.keys())
+print('Keys in a not in b:', a.keys() - b.keys())
+print('(key,value) pairs in common:', a.items() & b.items())
