6ad4082c8db238b35e8fc92c9c91bb0c428cb833
yandongxiao <yandongxiao@chinac.com>
2017-06-06 22:47:20 +0800

    creating_data_processing_pipelines

--- python-cookbook-master/src/4/creating_data_processing_pipelines/example.py
+++ python-cookbook-master/src/4/creating_data_processing_pipelines/example.py
@@ -9,14 +9,15 @@ def gen_find(filepat, top):
     '''
     Find all filenames in a directory tree that match a shell wildcard pattern
     '''
+    # 注意os.walk方法类似于find命令
     for path, dirlist, filelist in os.walk(top):
         for name in fnmatch.filter(filelist, filepat):
-            yield os.path.join(path,name)
+            yield os.path.join(path, name)
 
 def gen_opener(filenames):
     '''
     Open a sequence of filenames one at a time producing a file object.
-    The file is closed immediately when proceeding to the next iteration. 
+    The file is closed immediately when proceeding to the next iteration.
     '''
     for filename in filenames:
         if filename.endswith('.gz'):
@@ -26,6 +27,7 @@ def gen_opener(filenames):
         else:
             f = open(filename, 'rt')
         yield f
+        # 下一个文件对象返回时，上一次文件对象将会被关闭
         f.close()
 
 def gen_concatenate(iterators):
@@ -33,7 +35,12 @@ def gen_concatenate(iterators):
     Chain a sequence of iterators together into a single sequence.
     '''
     for it in iterators:
-        yield from it
+        # yield from it 简单的返回生成器 it 所产生的所有值
+        # it对象本身是file对象
+        # file对象本身是可迭代的
+        # file对象next方法输出的是一行一行的记录
+        # yield from it 返回这些文件的每一行一行
+        yield from it   # 为什么是yield from it
 
 def gen_grep(pattern, lines):
     '''
