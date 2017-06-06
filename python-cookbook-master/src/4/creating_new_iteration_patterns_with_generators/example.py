e2f88668e2ad364789a0e08c34af249355672de7
yandongxiao <yandongxiao@chinac.com>
2017-06-06 14:03:38 +0800

    creating_new_iteration_patterns_with_generators

--- python-cookbook-master/src/4/creating_new_iteration_patterns_with_generators/example.py
+++ python-cookbook-master/src/4/creating_new_iteration_patterns_with_generators/example.py
@@ -1,3 +1,6 @@
+# 一个函数中需要有一个 yield 语句即可将其转换为一个生成器
+# 不需要return语句
+# 生成器只能用于迭代操作
 def frange(start, stop, increment):
     x = start
     while x < stop:
---
+++ python-cookbook-master/src/4/creating_new_iteration_patterns_with_generators/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+def frange(start, end, step):
+    val = start
+    while val < end:
+        yield val
+        val += step
+
+for f in frange(0, 2, 0.5):
+    print f
