3f32be44bb698f5eb99a8cd4cc2439b61af5bb0d
yandongxiao <yandongxiao@chinac.com>
2017-06-07 10:06:00 +0800

    iterating_in_reverse

--- python-cookbook-master/src/4/iterating_in_reverse/test2.py
+++ python-cookbook-master/src/4/iterating_in_reverse/test2.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+def sub(l):
+    tmp = l[0]
+    l[0] -= 1
+    return tmp
+
+def add(l):
+    tmp = l[0]
+    l[0] += 1
+    return tmp
+
+
 class Countdown(object):
     def __init__(self, val):
         self._val = val
-
-    def sub(self):
-        tmp = self._val
-        self._val -= 1
-        return tmp
-
-    def add(self):
-        tmp = self._val
-        self._val += 1
-        return tmp
 
     # iter 函数一个鲜为人知的特性是它接受一个可选的 callable 对象
     # 和一个标记(结尾)值作为输入参数。
     # 当以这种方式使用的时候，它会创建一个迭代器， 这个迭代器会不断调用 callable 对象直到返回值和标记值相等为止。
     def __iter__(self):
-        return iter(self.sub, 0)
-        #while i >= 0:
-        #    yield i
-        #    i -= 1
+        # 使得sub函数之间需要传递共享的变量，所以需要利用引用传递
+        # 不再通过类成员变量来传递数据
+        l = [self._val]
+        return iter(lambda: sub(l), 0)
 
     # 注意是__reversed__, not __reverse__
     def __reversed__(self):
-        return iter(self.add, 5)
+        l = [1]
+        return iter(lambda: add(l), self._val+1)
 
 C = Countdown(5)
 for k in C:
     print k,
 print
 
-C = Countdown(0)
 for k in reversed(C):
     print k,
