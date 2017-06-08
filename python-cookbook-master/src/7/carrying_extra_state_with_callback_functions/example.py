a2807543346e2f65e2086816249e84cda398c1d0
yandongxiao <yandongxiao@chinac.com>
2017-06-08 15:22:31 +0800

    carrying_extra_state_with_callback_functions

--- python-cookbook-master/src/7/carrying_extra_state_with_callback_functions/example.py
+++ python-cookbook-master/src/7/carrying_extra_state_with_callback_functions/example.py
@@ -1,7 +1,13 @@
-#! /usr/bin/python
+#! /usr/bin/env python3
 # This example is about the problem of carrying extra state around
 # through callback functions.   To test the examples, this very
 # simple code emulates the typical control of a callback.
+
+# 回调函数使用外部变量的四种方法：
+# 1. 类
+# 2. closure
+# 3. coroutine
+# 4. from functools import partial
 
 def apply_async(func, args, *, callback):
     # Compute the result
@@ -25,11 +31,12 @@ apply_async(add, (2, 3), callback=print_result)
 apply_async(add, ('hello', 'world'), callback=print_result)
 
 # (b) Using a bound method
-
+# 让回调函数访问更多的外部变量的方式是：传递一个方法
 print('# --- Using a bound-method')
 
 class ResultHandler:
     def __init__(self):
+        # 外部变量被封装在类实例的成员当中
         self.sequence = 0
     def handler(self, result):
         self.sequence += 1
@@ -40,13 +47,13 @@ apply_async(add, (2, 3), callback=r.handler)
 apply_async(add, ('hello', 'world'), callback=r.handler)
 
 # (c) Using a closure
-
+# 如果一个类只有一个方法，那么可以通过闭包函数解决
 print('# --- Using a closure')
 
 def make_handler():
     sequence = 0
     def handler(result):
-        nonlocal sequence
+        nonlocal sequence   # 否则就是use before assignment
         sequence += 1
         print('[{}] Got: {}'.format(sequence, result))
     return handler
@@ -56,25 +63,25 @@ apply_async(add, (2, 3), callback=handler)
 apply_async(add, ('hello', 'world'), callback=handler)
 
 
-# (d) Using a coroutine
+# (d) Using a coroutine(协程)
 
 print('# --- Using a coroutine')
-
+# 它显得更加简洁，因为总共就一个函数而已
 def make_handler():
     sequence = 0
     while True:
-        result = yield
+        result = yield  # 可以不使用括号
         sequence += 1
         print('[{}] Got: {}'.format(sequence, result))
 
 handler = make_handler()
-next(handler)    # Advance to the yield
-
+next(handler)    # 启动协程
 apply_async(add, (2, 3), callback=handler.send)
-apply_async(add, ('hello', 'world'), callback=handler.send)
+apply_async(add, ('hello', 'world'), callback=handler.send) # 使用send作为回调函数
+# NOTE: 没有关闭协程
 
 # (e) Partial function application
-
+# 没毛病
 print('# --- Using partial')
 
 class SequenceNo:
@@ -91,5 +98,3 @@ from functools import partial   #So,partial is used to pass a argument to other 
 apply_async(add, (2, 3), callback=partial(handler, seq=seq))
 #apply_async(add, (2, 3), seq.handler)
 apply_async(add, ('hello', 'world'), callback=partial(handler, seq=seq))
-
-
---
+++ python-cookbook-master/src/7/note/coroutine.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# python的协程可以类比LUA中的协程
+# 协程最重要的特点是多个协程是在同一个线程下运行
+
+def grep(pattern):
+    # 生成器和协程都是用了yield关键字
+    while True:
+        line = yield
+        if pattern in line:
+            print line,
+
+search = grep("print")
+next(search)    # 启动协程
+
+for line in open("lambda.py"):
+    # send方法传递line给协程，并触发它运行
+    # 这里grep确实没有什么必要实现成协程，实现为函数即可
+    search.send(line)
+
+search.close()
