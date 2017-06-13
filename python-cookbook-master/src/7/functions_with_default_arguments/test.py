c11150608cf9f1ce96b76451cee9eb436a98a2f1
yandongxiao <yandongxiao@chinac.com>
2017-06-13 16:45:03 +0800

    functions with default arguments

--- python-cookbook-master/src/7/functions_with_default_arguments/test.py
+++ python-cookbook-master/src/7/functions_with_default_arguments/test.py
@@ -1,21 +1,43 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-def get(b = []):
-    b.append(1)
-    print b
+# NOTE: 不要使用可变变量作为函数参数的默认值
+def get(v = []):
+    v.append(1)
+    return v
+assert get() == [1]
+assert get() == [1, 1]
 
-get()
-get()
-get()
+# NOTE: 允许传递给默认参数一个变量
+# NOTE：定义函数是变量的值是多少，默认参数的值就是多少。后面变量的值的改变，不会影响默认参数
+# NOTE：第二条的成立，当然建立在变量是不可修改变量了
+arg = 1
+def get(v=arg):
+    return v
+arg=2
+assert get() == 1
 
+
+# NOTE: 如果希望两个closure相互独立，就不要共享v
+def add():
+    def func(a):
+        return v + a
+    return func
+
+l = []
+for v in [1, 2, 3]:
+    l.append(add())
+for i in l:
+    assert i(10) == 13
+
+# 如果None也做为了一个用户的输入的情况
 _obj = object()
-def set(b = _obj):
-    if b is _obj:
-        print "no arg"
+def get(arg=_obj):
+    if arg is _obj:
+        print "no argument"
     else:
-        print b
+        print arg
 
-set()
-set(None)
-set(1)
+get()
+get(None)
+get(0)
