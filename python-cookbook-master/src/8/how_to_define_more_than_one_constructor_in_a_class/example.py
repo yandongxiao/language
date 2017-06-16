e91fadca457c99a49c6e865022aa70ec16c1c190
yandongxiao <yandongxiao@chinac.com>
2017-06-16 10:35:02 +0800

    how to define more than one constructor in a class

--- python-cookbook-master/src/8/how_to_define_more_than_one_constructor_in_a_class/example.py
+++ python-cookbook-master/src/8/how_to_define_more_than_one_constructor_in_a_class/example.py
@@ -1,3 +1,8 @@
+#! /usr/bin/env python3
+#encoding: utf-8
+
+# 通过类方法定义新的构造方法
+
 import time
 
 class Date:
@@ -11,7 +16,7 @@ class Date:
     @classmethod
     def today(cls):
         t = time.localtime()
-        return cls(t.tm_year, t.tm_mon, t.tm_mday)
+        return cls(t.tm_year, t.tm_mon, t.tm_mday)      # 本质上还是调用了__init__方法
 
 if __name__ == '__main__':
     a = Date(2012, 12, 21)
@@ -19,10 +24,11 @@ if __name__ == '__main__':
     print(a.year, a.month, a.day)
     print(b.year, b.month, b.day)
 
+    # 需要了解classmethod和staticmethod是如何继承的
     class NewDate(Date):
         pass
 
     c = Date.today()
     d = NewDate.today()
-    print('Should be Date instance:', Date)
-    print('Should be NewDate instance:', NewDate)
+    print('Should be Date instance:', c)
+    print('Should be NewDate instance:', d)
