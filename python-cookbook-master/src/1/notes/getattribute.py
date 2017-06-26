e19222388c773a849beeef107ebb617d2b360892
yandongxiao <yandongxiao@chinac.com>
2017-06-26 17:32:54 +0800

    update

--- python-cookbook-master/src/1/notes/getattribute.py
+++ python-cookbook-master/src/1/notes/getattribute.py
@@ -1,18 +1,36 @@
-#! /usr/bin/python
+#! /usr/bin/env python3
 
-class Test:
+class A(object):
+    def __init__(self):
+        self.num2 = 100
+
+class Test(A):
     def __init__(self):
         self.num  = 10
+        super().__init__()
 
     def __getitem__(self, key):
         print("__getitem__ called")
         if key == 'num':
             return 10     #called
 
+    def __getattr__(self, key):
+        print("__getattr__ called")
+        return "nihao"
+
+    def __setattr__(self, key, val):
+        print("__setattr__ called")
+        object.__setattr__(self, key, val)
+
     def __getattribute__(self, key):
         print("__getattribute__ called")
-        return self.num     # wiil create an error
+        return object.__getattribute__(self, key)    # wiil create an error
 
 tt = Test()
 #assert getattr(tt, 'num') == 10     # __getattribute__ called
 num = tt.num = 20
+print(tt.num)
+print(tt["num"])
+print(tt.num2)
+
+
