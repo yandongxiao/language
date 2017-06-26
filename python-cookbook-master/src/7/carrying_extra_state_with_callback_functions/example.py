9697b715638dc18c3bb98231c09648282efc7512
yandongxiao <yandongxiao@chinac.com>
2017-06-26 10:52:01 +0800

    carring extra state with callback functions

--- python-cookbook-master/src/7/carrying_extra_state_with_callback_functions/example.py
+++ python-cookbook-master/src/7/carrying_extra_state_with_callback_functions/example.py
@@ -1,13 +1,11 @@
 #! /usr/bin/env python3
-# This example is about the problem of carrying extra state around
-# through callback functions.   To test the examples, this very
-# simple code emulates the typical control of a callback.
 
 # 回调函数使用外部变量的四种方法：
-# 1. 类
-# 2. closure
-# 3. coroutine
-# 4. from functools import partial
+# 1. 不携带任何外部变量
+# 2. 传递一个bound方法
+# 3. 传递一个闭包
+# 4. 传递一个协程
+# 5. 利用partial函数，对回调函数接口二次封装
 
 def apply_async(func, args, *, callback):
     # Compute the result
@@ -21,19 +19,15 @@ def add(x, y):
     return x + y
 
 # (a) A simple callback example
-
 print('# --- Simple Example')
-
 def print_result(result):
     print("Got:", result)
-
 apply_async(add, (2, 3), callback=print_result)
 apply_async(add, ('hello', 'world'), callback=print_result)
 
 # (b) Using a bound method
 # 让回调函数访问更多的外部变量的方式是：传递一个方法
 print('# --- Using a bound-method')
-
 class ResultHandler:
     def __init__(self):
         # 外部变量被封装在类实例的成员当中
@@ -41,7 +35,6 @@ class ResultHandler:
     def handler(self, result):
         self.sequence += 1
         print('[{}] Got: {}'.format(self.sequence, result))
-
 r = ResultHandler()
 apply_async(add, (2, 3), callback=r.handler)
 apply_async(add, ('hello', 'world'), callback=r.handler)
@@ -49,7 +42,6 @@ apply_async(add, ('hello', 'world'), callback=r.handler)
 # (c) Using a closure
 # 如果一个类只有一个方法，那么可以通过闭包函数解决
 print('# --- Using a closure')
-
 def make_handler():
     sequence = 0
     def handler(result):
@@ -57,14 +49,12 @@ def make_handler():
         sequence += 1
         print('[{}] Got: {}'.format(sequence, result))
     return handler
-
 handler = make_handler()
 apply_async(add, (2, 3), callback=handler)
 apply_async(add, ('hello', 'world'), callback=handler)
 
 
 # (d) Using a coroutine(协程)
-
 print('# --- Using a coroutine')
 # 它显得更加简洁，因为总共就一个函数而已
 def make_handler():
@@ -78,23 +68,21 @@ handler = make_handler()
 next(handler)    # 启动协程
 apply_async(add, (2, 3), callback=handler.send)
 apply_async(add, ('hello', 'world'), callback=handler.send) # 使用send作为回调函数
-# NOTE: 没有关闭协程
+handler.close()
 
 # (e) Partial function application
-# 没毛病
 print('# --- Using partial')
-
 class SequenceNo:
     def __init__(self):
         self.sequence = 0
 
-def handler(result, seq):       #it is not a function of SequenceNo
+def handler(result, seq):
     seq.sequence += 1
     print('[{}] Got: {}'.format(seq.sequence, result))
 
 seq = SequenceNo()
 from functools import partial   #So,partial is used to pass a argument to other fucntion.
+new_handler = partial(handler, seq=seq)
 
-apply_async(add, (2, 3), callback=partial(handler, seq=seq))
-#apply_async(add, (2, 3), seq.handler)
-apply_async(add, ('hello', 'world'), callback=partial(handler, seq=seq))
+apply_async(add, (2, 3), callback=new_handler)
+apply_async(add, ('hello', 'world'), callback=new_handler)
