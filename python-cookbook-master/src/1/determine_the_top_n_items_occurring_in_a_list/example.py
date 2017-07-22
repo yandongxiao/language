b4d999ac2d5513367e4dbe5184c6709833bb883d
yandongxiao <yandongxiao@chinac.com>
2017-07-22 11:07:12 +0800

    defaultdict range heapq counter

--- python-cookbook-master/src/1/determine_the_top_n_items_occurring_in_a_list/example.py
+++ python-cookbook-master/src/1/determine_the_top_n_items_occurring_in_a_list/example.py
@@ -1,7 +1,5 @@
-#! /usr/bin/python
-# example.py
-#
-# Determine the most common words in a list
+#!/usr/bin/env python
+# encoding: utf-8
 
 words = [
    'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
@@ -10,17 +8,40 @@ words = [
    'my', 'eyes', "you're", 'under'
 ]
 
+# method-1
+counter = {}
+for word in words:
+    if counter.has_key(word):
+        counter[word] += 1
+    else:
+        counter[word] = 1
+
+sorted_keys = sorted(counter, key=lambda k: counter[k], reverse=True)
+for i in range(0, 3):
+    key = sorted_keys[i]
+    print key, counter[key]
+
+# method-2
+from collections import defaultdict
+counter = defaultdict(int)
+for word in words:
+    counter[word] += 1
+
+for i in range(0, 3):
+    k1 = max(counter, key=lambda k: counter[k])
+    print k1, counter[k1]
+    del counter[k1]
+
+# method-3
+from collections import defaultdict
+import heapq
+
+counter = defaultdict(int)
+for word in words: counter[word] += 1
+keys = heapq.nlargest(3, counter, key=lambda k: counter[k])
+print [(k, counter[k]) for k in keys]
+
+# method 4
 from collections import Counter
-word_counts = Counter(words)
-top_three = word_counts.most_common(3)
-print(top_three)
-# outputs [('eyes', 8), ('the', 5), ('look', 4)]
-
-# Example of merging in more words
-
-morewords = ['why','are','you','not','looking','in','my','eyes']
-word_counts.update(morewords)
-print(word_counts.most_common(3))
-
-
-
+counter = Counter(words)
+print counter.most_common(3)
