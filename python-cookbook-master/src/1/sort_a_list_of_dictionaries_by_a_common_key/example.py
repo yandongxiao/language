106cd8638bb7171ee1b958ccce4f6cd595d61418
yandongxiao <yandongxiao@chinac.com>
2017-07-22 18:01:34 +0800

    from operator import itemgetter

--- python-cookbook-master/src/1/sort_a_list_of_dictionaries_by_a_common_key/example.py
+++ python-cookbook-master/src/1/sort_a_list_of_dictionaries_by_a_common_key/example.py
@@ -1,8 +1,7 @@
-#! /usr/bin/python
-# example.py
-#
-# Sort a list of a dicts on a common key
+#!/usr/bin/env python
+# encoding: utf-8
 
+# method - 1
 rows = [
     {'fname': 'Brian', 'lname': 'Jones', 'uid': 1003},
     {'fname': 'David', 'lname': 'Beazley', 'uid': 1002},
@@ -10,19 +9,30 @@ rows = [
     {'fname': 'Big', 'lname': 'Jones', 'uid': 1004}
 ]
 
+a1 = sorted(rows, key=lambda item: item['fname'])
+a2 = sorted(rows, key=lambda item: item['uid'])
+a3 = sorted(rows, key=lambda item: (item['uid'], item['fname']))
+
+# method - 2
 from operator import itemgetter
+b1 = sorted(rows, key=itemgetter('fname'))
+b2 = sorted(rows, key=itemgetter('uid'))
+b3 = sorted(rows, key=itemgetter('uid', 'fname'))
+assert(a1==b1)
+assert(a2==b2)
+assert(a3==b3)
 
-rows_by_fname = sorted(rows, key=itemgetter('fname'))
-rows_by_uid = sorted(rows, key=itemgetter('uid'))
+# method - 3
+# item is a tuple, you do not need to assign it.
+class itemgetter:
+    def __init__(self, item, *items):
+        self.items = (item, ) + items
+    def __call__(self, obj):
+        return tuple(obj[x] for x in self.items)
 
-from pprint import pprint
-
-print("Sorted by fname:")
-pprint(rows_by_fname)
-
-print("Sorted by uid:")
-pprint(rows_by_uid)
-
-rows_by_lfname = sorted(rows, key=itemgetter('lname', 'fname'))
-print("Sorted by lname,fname:")
-pprint(rows_by_lfname)
+c1 = sorted(rows, key=itemgetter('fname'))
+c2 = sorted(rows, key=itemgetter('uid'))
+c3 = sorted(rows, key=itemgetter('uid', 'fname'))
+assert(a1==c1)
+assert(a2==c2)
+assert(a3==c3)
