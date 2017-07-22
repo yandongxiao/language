458b7a94944fc4a8a6d728ac6b80e63f8d56d03b
yandongxiao <yandongxiao@chinac.com>
2017-07-22 21:27:02 +0800

    from operator import attrgetter

--- python-cookbook-master/src/1/sort_objects_without_native_comparison_support/example.py
+++ python-cookbook-master/src/1/sort_objects_without_native_comparison_support/example.py
@@ -1,23 +1,5 @@
-#! /usr/bin/python
+#!/usr/bin/env python
 # encoding: utf-8
-
-# attrgetter的实现方式
-
-class attrgetter :
-    def __init__(self, attr, *attrs):
-        if not attrs:
-            if not isinstance(attr, str):
-                raise TypeError("attribute name must be a string")
-            names = attr.split('.')
-            def func(obj):
-                for name in names:
-                    obj = getattr(obj,name)
-                return obj
-            self._call = func
-
-    def __call__(self, obj):
-        return self._call(obj)
-
 
 class User:
     def __init__(self, user_id):
@@ -25,9 +7,13 @@ class User:
     def __repr__(self):
         return 'User({})'.format(self.user_id)
 
-# Example
 users = [User(23), User(3), User(99)]
-print(users)
 
-# Sort it by user-id
-print(sorted(users, key=attrgetter('user_id')))
+
+# method - 1
+print sorted(users, key=lambda user: user.user_id)
+
+# method - 2
+from operator import attrgetter
+getter = attrgetter("user_id")
+print sorted(users, key=attrgetter("user_id"))
