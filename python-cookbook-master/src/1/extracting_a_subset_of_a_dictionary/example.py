544fae8a4646ddecad91ca98e3233719174be3e2
yandongxiao <yandongxiao@chinac.com>
2017-07-22 11:31:15 +0800

    dict comprehensions items

--- python-cookbook-master/src/1/extracting_a_subset_of_a_dictionary/example.py
+++ python-cookbook-master/src/1/extracting_a_subset_of_a_dictionary/example.py
@@ -1,32 +1,21 @@
-#! /usr/bin/python
+#!/usr/bin/env python
 # encoding: utf-8
 
-# example of extracting a subset from a dictionary
-from pprint import pprint
 
 prices = {
-   'ACME': 45.23,
-   'AAPL': 612.78,
-   'IBM': 205.55,
-   'HPQ': 37.20,
-   'FB': 10.75
+    'ACME': 45.23,
+    'AAPL': 612.78,
+    'IBM': 205.55,
+    'HPQ': 37.20,
+    'FB': 10.75
 }
 
 # Make a dictionary of all prices over 200
-p1 = { key:value for key, value in prices.items() if value > 200 }
+print {key:prices[key] for key in prices if prices[key] > 200}
+print {key:val for key, val in prices.items() if val > 200}
 
-print("All prices over 200")
-pprint(p1)
-print(p1)
 
 # Make a dictionary of tech stocks
 tech_names = { 'AAPL', 'IBM', 'HPQ', 'MSFT' }
-p2 = { key:value for key,value in prices.items() if key in tech_names }
-
-# 大多数情况下字典推导能做到的，通过创建一个元组序列然后把它传给 dict() 函数也能实现
-p3 = dict((key, value) for key, value in prices.items() if key in tech_names)
-
-print("All techs")
-print(p2)
-print("-----------")
-print(p3)
+print {key:prices[key] for key in prices if key in tech_names}
+print dict((key, prices[key]) for key in prices if key in tech_names)
