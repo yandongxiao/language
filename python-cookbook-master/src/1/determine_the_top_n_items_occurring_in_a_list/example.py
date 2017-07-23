c6ebd307faebb074d1227fa8009e7ec59f4b187c
yandongxiao <yandongxiao@chinac.com>
2017-07-23 22:21:03 +0800

    sorted, from collections import defaultdict, Counter

--- python-cookbook-master/src/1/determine_the_top_n_items_occurring_in_a_list/example.py
+++ python-cookbook-master/src/1/determine_the_top_n_items_occurring_in_a_list/example.py
@@ -1,47 +1,57 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-words = [
-   'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
-   'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around', 'the',
-   'eyes', "don't", 'look', 'around', 'the', 'eyes', 'look', 'into',
-   'my', 'eyes', "you're", 'under'
+"""
+    Determine the top-n items occurring in a list
+"""
+
+WORDS = [
+    'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
+    'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around', 'the',
+    'eyes', "don't", 'look', 'around', 'the', 'eyes', 'look', 'into',
+    'my', 'eyes', "you're", 'under'
 ]
 
-# method-1
-counter = {}
-for word in words:
-    if counter.has_key(word):
+def doit():
+    """ doit """
+    # method - 1
+    counter = {}
+    for word in WORDS:
+        if counter.has_key(word):
+            counter[word] += 1
+        else:
+            counter[word] = 1
+
+    sorted_keys = sorted(counter, key=lambda k: counter[k], reverse=True)
+    for i in range(0, 3):
+        key = sorted_keys[i]
+        print key, counter[key]
+
+    # method - 2
+    from collections import defaultdict
+    counter = defaultdict(int)
+    for word in WORDS:
         counter[word] += 1
-    else:
-        counter[word] = 1
 
-sorted_keys = sorted(counter, key=lambda k: counter[k], reverse=True)
-for i in range(0, 3):
-    key = sorted_keys[i]
-    print key, counter[key]
+    for i in range(0, 3):
+        key = max(counter, key=lambda k: counter[k])
+        print key, counter[key]
+        del counter[key]
 
-# method-2
-from collections import defaultdict
-counter = defaultdict(int)
-for word in words:
-    counter[word] += 1
+    # method - 3
+    import heapq
 
-for i in range(0, 3):
-    k1 = max(counter, key=lambda k: counter[k])
-    print k1, counter[k1]
-    del counter[k1]
+    counter = defaultdict(int)
+    for word in WORDS:
+        counter[word] += 1
+    keys = heapq.nlargest(3, counter, key=lambda k: counter[k])
+    print [(k, counter[k]) for k in keys]
 
-# method-3
-from collections import defaultdict
-import heapq
+    # method - 4
+    from collections import Counter
+    counter = Counter(WORDS)
+    print counter.most_common(3)
 
-counter = defaultdict(int)
-for word in words: counter[word] += 1
-keys = heapq.nlargest(3, counter, key=lambda k: counter[k])
-print [(k, counter[k]) for k in keys]
 
-# method 4
-from collections import Counter
-counter = Counter(words)
-print counter.most_common(3)
+if __name__ == "__main__":
+    doit()
