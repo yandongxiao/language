95bd6c3031bf9d731392900d8eb309c0cd374cdc
yandongxiao <yandongxiao@chinac.com>
2017-07-23 23:12:13 +0800

    implementing a priority queue

--- python-cookbook-master/src/1/implementing_a_priority_queue/example.py
+++ python-cookbook-master/src/1/implementing_a_priority_queue/example.py
@@ -1,37 +1,38 @@
 #! /usr/bin/env python
 # encoding: utf-8
 
-# example.py
-#
-# Example of a priority queue
+"""
+    Implementing a priority queue
+"""
+
+__all__ = ['PriorityQueue']
 
 import heapq
 
-class PriorityQueue:
+
+class PriorityQueue(object):
+    """List that has priority"""
+
     def __init__(self):
         self._queue = []
         self._index = 0
 
     def push(self, item, priority):
+        """push an item with priority"""
         # heapq 本身并不支持key关键字，需要借助元组的特殊比较方式来实现
         heapq.heappush(self._queue, (-priority, self._index, item))
         self._index += 1
 
     def pop(self):
+        """pop an item with leastest priority"""
         return heapq.heappop(self._queue)[-1]
 
-# Example use
-class Item:
-    def __init__(self, name):
-        self.name = name
-    def __repr__(self):
-        return 'Item({!r})'.format(self.name)
 
 q = PriorityQueue()
-q.push(Item('foo'), 1)
-q.push(Item('bar'), 5)
-q.push(Item('spam'), 4)
-q.push(Item('grok'), 1)
+q.push('foo', 1)
+q.push('bar', 5)
+q.push('spam', 4)
+q.push('grok', 1)
 
 print("Should be bar:", q.pop())
 print("Should be spam:", q.pop())
