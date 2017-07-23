ce8449cc5f3e200d94824626fdd1565b7ad49ed7
yandongxiao <yandongxiao@chinac.com>
2017-07-23 23:20:09 +0800

    keep the last n item

--- python-cookbook-master/src/1/keeping_the_last_n_items/example.py
+++ python-cookbook-master/src/1/keeping_the_last_n_items/example.py
@@ -1,29 +1,38 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+"""
+    Keeping the last n items.
+"""
+
+import fnmatch
 from collections import deque
 
-# method - 1
-def search1(lines, keyword, maxlen = 3):
-    queue = deque(maxlen=maxlen)
-    for line in lines:
-        if keyword in line:
-            yield line, queue
-        queue.append(line)
-
-# method - 2
-import fnmatch
-def search2(lines, keyword, maxlen = 3):
-    queue = deque(maxlen=maxlen)
-    for line in lines:
-        if fnmatch.fnmatch(line, keyword):
-            yield line, queue
-        else:
+def doit():
+    """doit"""
+    # method - 1
+    def search1(lines, keyword, maxlen=3):
+        """Search by using in expression."""
+        queue = deque(maxlen=maxlen)
+        for line in lines:
+            if keyword in line:
+                yield line, queue
             queue.append(line)
 
-# with as 的工作原理：see http://blog.kissdata.com/2014/05/23/python-with.html
-# 1. 捕获异常；2. 关闭句柄
-with open("somefile.txt") as lines:
-    for line, prevlines in search2(lines, "* to *"):
-        print line, prevlines
+    # method - 2
+    def search2(lines, keyword, maxlen=3):
+        """Search by using fnmatch."""
+        queue = deque(maxlen=maxlen)
+        for line in lines:
+            if fnmatch.fnmatch(line, keyword):
+                yield line, queue
+            else:
+                queue.append(line)
 
+    # with as 的工作原理：see http://blog.kissdata.com/2014/05/23/python-with.html
+    # 1. 捕获异常；2. 关闭句柄
+    with open("somefile.txt") as lines:
+        for line, prev_lines in search2(lines, "* to *"):
+            print line, prev_lines
+
+doit()
