9d01ece546f86e5672faf8e904fbdbbd6c6aa04c
yandongxiao <yandongxiao@chinac.com>
2017-07-24 11:05:10 +0800

    re.match, re.findall, re.finditer, re.compile

--- python-cookbook-master/src/2/matching_and_searching_for_text_patterns_using_regular_expressions/example.py
+++ python-cookbook-master/src/2/matching_and_searching_for_text_patterns_using_regular_expressions/example.py
@@ -9,14 +9,25 @@ import re
 text = 'Today is 11/27/2012. PyCon starts 3/13/2013.'
 
 # (a) Find all matching dates
-datepat = re.compile(r'\d+/\d+/\d+')
-print(datepat.findall(text))
+date = '11/27/2012.'
+datepat = r'\d+/\d+/\d+'
+assert re.match(datepat, text) is None
+assert re.match(datepat, date) is not None
+
+pattern = re.compile(datepat)
+assert pattern.match(text) is None
+assert pattern.match(date) is not None
+
+assert re.findall(datepat, text) is not None
+assert pattern.findall(text) is not None
 
 # (b) Find all matching dates with capture groups
+# [(11, 27, 2012), (3, 13, 2013)]
 datepat = re.compile(r'(\d+)/(\d+)/(\d+)')
 for month, day, year in datepat.findall(text):
     print('{}-{}-{}'.format(year, month, day))
 
 # (c) Iterative search
 for m in datepat.finditer(text):
-    print(m.groups())
+    m, d, y = m.groups()
+    print('{}-{}-{}'.format(y, m, d))
