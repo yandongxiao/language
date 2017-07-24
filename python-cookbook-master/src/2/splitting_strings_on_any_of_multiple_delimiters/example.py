a79ef54871c20e3f71955f626461376fb9e77647
yandongxiao <yandongxiao@chinac.com>
2017-07-24 10:29:33 +0800

    import re, re.split(r'[\s;,]\s*')

--- python-cookbook-master/src/2/splitting_strings_on_any_of_multiple_delimiters/example.py
+++ python-cookbook-master/src/2/splitting_strings_on_any_of_multiple_delimiters/example.py
@@ -1,8 +1,11 @@
 #! /usr/bin/python
-# example.py
-#
-# Example of splitting a string on multiple delimiters using a regex
+# encoding: utf-8
 
+"""
+    split a string by using re module
+"""
+
+from __future__ import print_function
 import re
 
 line = 'asdf fjdk; afed, fjek,asdf,      foo'
@@ -21,10 +24,10 @@ delimiters = fields[1::2]
 delimiters.append('')
 print('value =', values)
 print('delimiters =', delimiters)
-newline = ''.join(v+d for v,d in zip(values, delimiters))
+newline = ''.join(v+d for v, d in zip(values, delimiters))
 print('newline =', newline)
 
 # (d) Splitting using a non-capture group
+# 使用圆括号进行分组但是不进行捕获
 parts = re.split(r'(?:,|;|\s)\s*', line)
 print(parts)
-
