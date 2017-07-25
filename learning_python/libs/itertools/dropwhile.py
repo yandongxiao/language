f20e137013e8a291799e751568d6d7f13646bb0c
yandongxiao <yandongxiao@chinac.com>
2017-07-25 10:19:10 +0800

    from itertools import dropwhile

--- learning_python/libs/itertools/dropwhile.py
+++ learning_python/libs/itertools/dropwhile.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from itertools import dropwhile
+import itertools
 
-# 直到找到第一个大于等于5的元素，然后遍历后续的元素
-for k in dropwhile(lambda x: x<5, [4,5,6,3,8]):
-    print k
+assert list(itertools.dropwhile(lambda x: x<5, [4, 5, 6, 3, 8])) == [5, 6, 3, 8]
