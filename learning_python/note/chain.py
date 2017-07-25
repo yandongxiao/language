38d12786035f00321d23a0685a8415c23b3d13ce
yandongxiao <yandongxiao@chinac.com>
2017-07-25 10:51:46 +0800

    from collections import zip, from collections import chain

--- learning_python/note/chain.py
+++ learning_python/note/chain.py
@@ -6,11 +6,10 @@ from itertools import chain
 A = [1, 2, 3, 4]
 B = ["a", "b", "c", "d"]
 
-# chain用于避免对不同的迭代对象调用多次循环操作
+# chain 用于避免对不同的迭代对象调用多次循环操作
 # chain 更加节省内存
 for x in chain(A, B):
     print x
-
 
 for i in chain("hello", "world"):
     print i,
