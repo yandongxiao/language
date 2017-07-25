7e1605d4ec4d3998f004a4f14155b48668fe68d6
yandongxiao <yandongxiao@chinac.com>
2017-07-25 11:20:03 +0800

    import heapq;  heapq.merge(seq1, seq2)

--- python-cookbook-master/src/4/iterating_in_sorted_order_over_merged_sorted_iterables/example.py
+++ python-cookbook-master/src/4/iterating_in_sorted_order_over_merged_sorted_iterables/example.py
@@ -5,4 +5,3 @@ a = [1, 4, 7, 10]
 b = [2, 5, 6, 11]
 for c in heapq.merge(a, b):
     print(c)
-
--- python-cookbook-master/src/4/iterating_in_sorted_order_over_merged_sorted_iterables/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
-import heapq
-
-# 这种排序算法称为归并排序
-l1 = [1, 3, 5, 7]
-l2 = [2, 4, 6, 8]
-print list(heapq.merge(l1, l2))
