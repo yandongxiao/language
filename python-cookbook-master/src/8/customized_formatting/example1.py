1fe8d327fb2825fd3e19e10324c2fecf4e9ac643
yandongxiao <yandongxiao@chinac.com>
2017-07-26 14:42:24 +0800

    format支持：普通模式，关键字模式，实例属性等方式

--- python-cookbook-master/src/8/customized_formatting/example1.py
+++ python-cookbook-master/src/8/customized_formatting/example1.py
@@ -1,3 +1,27 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+# format方式自定义格式化字符串
+# 支持普通模式，位置模式，关键字模式，实例属性模式
+# 总之，支持变量插入模式
+
+# 普通模式
+'{} is {}'.format(1,2) == '1 is 2'
+
+# 支持位置
+'{1} is {0}'.format(1,2) == '2 is 1'
+
+# 支持关键字
+'{name} is {age}'.format(name='jack', age=10) == 'jack is 10'
+
+# 支持类属性
+class Person(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+'{p.name} is {p.age}'.format(p=Person('jack', 10)) == 'jack is 10'
+
+
 _formats = {
     'ymd' : '{d.year}-{d.month}-{d.day}',
     'mdy' : '{d.month}/{d.day}/{d.year}',
@@ -10,8 +34,14 @@ class Date:
         self.month = month
         self.day = day
 
-    def __format__(self, code):
+    def __format__(self, code=''):
+        print "dsadsall", code
         if code == '':
             code = 'ymd'
         fmt = _formats[code]
         return fmt.format(d=self)
+
+d = Date(11, 11, 11)
+#print format(date)
+#print '{d.year}-{d.month}-{d.day}'.format(d=d)
+print '{}'.format(d)
