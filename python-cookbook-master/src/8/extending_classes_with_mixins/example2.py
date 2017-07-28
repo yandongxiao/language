9513360d5749f2683d1efdb0796fb9d49d294c26
yandongxiao <yandongxiao@chinac.com>
2017-07-28 17:43:20 +0800

    实现一个有状态的混入类。另外：__init__最好有super().__init__

--- python-cookbook-master/src/8/extending_classes_with_mixins/example2.py
+++ python-cookbook-master/src/8/extending_classes_with_mixins/example2.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python3
 # encoding: utf-8
 
-# 与example1中关于混入类有些出入, example1中说的是混入类不能有自己的实例属性和__init__方法
-class RestrictKeysMixin:
+
+class RestrictKeysMixin(object):
+    # 这个混入类是有状态的
+    # 虽然RDict没有__init__方法，但是默认实现的方式会去调用基类的方法
     def __init__(self, *args, _restrict_key_type, **kwargs):
         self.__restrict_key_type = _restrict_key_type
+        # 这一句是必须的哟
         super().__init__(*args, **kwargs)
 
     def __setitem__(self, key, value):
@@ -12,10 +15,10 @@ class RestrictKeysMixin:
             raise TypeError('Keys must be ' + str(self.__restrict_key_type))
         super().__setitem__(key, value)
 
-# Example
 
 class RDict(RestrictKeysMixin, dict):
     pass
+
 
 d = RDict(_restrict_key_type=str)
 e = RDict([('name','Dave'), ('n',37)], _restrict_key_type=str)
