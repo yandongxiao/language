3e7b020586a0aad8d6a5b4d11072ccb57ff9a63e
yandongxiao <yandongxiao@chinac.com>
2017-07-29 22:49:21 +0800

    __getattribute__特殊方法的封装是在类上进行的，这样做的主要原因是
    类对象并没有重写__getattribute__方法，装饰器也就没有办法安装在她的上边

--- python-cookbook-master/src/9/monkeypatching_class_definitions/example.py
+++ python-cookbook-master/src/9/monkeypatching_class_definitions/example.py
@@ -1,3 +1,10 @@
+#! /usr/bin/env python3
+# encoding: utf-8
+
+import functools
+
+# 不需要进行wrpas操作，因为还是返回了原先的类对象
+# 只是修改了一个属性而已
 def log_getattribute(cls):
     # Get the original implementation
     orig_getattribute = cls.__getattribute__
@@ -11,11 +18,22 @@ def log_getattribute(cls):
     cls.__getattribute__ = new_getattribute
     return cls
 
+
+def wrapper(func):
+    @functools.wraps(func)
+    def print_func(*argv, **kwargv):
+        print(func.__name__)
+        return func(*argv, **kwargv)
+    return print_func
+
 # Example use
 @log_getattribute
 class A:
+    # __init__ = wrapper(__init__)
+    @wrapper
     def __init__(self,x):
         self.x = x
+
     def spam(self):
         pass
 
