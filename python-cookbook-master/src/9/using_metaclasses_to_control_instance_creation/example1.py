e126db1f243f0dc36ba152b5ffc13aa2d7a2f0fa
yandongxiao <yandongxiao@chinac.com>
2017-07-29 21:39:30 +0800

    元类的__call__方法，可以用来阻止类对象进行实例化

--- python-cookbook-master/src/9/using_metaclasses_to_control_instance_creation/example1.py
+++ python-cookbook-master/src/9/using_metaclasses_to_control_instance_creation/example1.py
@@ -1,20 +1,20 @@
 #! /usr/bin/env python3
 # encoding: utf-8
 
-# example1.py
-#
-# Not allowing direct instantiation
-
+# 这是一个元类
 class NoInstances(type):
     # 这里的__call__不是应用在实例对象上的，而是类对象上的
     # 所以，不允许类对象创建实例
+    # 类对象指定调用()，是实例化对象的过程，也会调用元类的__call__方法
     def __call__(self, *args, **kwargs):
         raise TypeError("Can't instantiate directly")
+
 
 class Spam(metaclass=NoInstances):
     @staticmethod
     def grok(x):
         print('Spam.grok')
+
 
 if __name__ == '__main__':
     try:
