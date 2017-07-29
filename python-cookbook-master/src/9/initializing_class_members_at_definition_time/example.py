3db542a19b1251d368e5dff00318e339e0ec7c6f
yandongxiao <yandongxiao@chinac.com>
2017-07-29 22:23:02 +0800

    元类__init__方式是在类对象创建后被调用的第一个方法
    setattr(cls, name, property(operator.getiterm(n)))
    class Foo(tuple, StructTuple)

--- python-cookbook-master/src/9/initializing_class_members_at_definition_time/example.py
+++ python-cookbook-master/src/9/initializing_class_members_at_definition_time/example.py
@@ -1,17 +1,24 @@
+#! /usr/bin/env python3
+# encoding: utf-8
+
 import operator
 
 class StructTupleMeta(type):
+    # __init__ 调用时类对象已经被创建
+    # 注意__init__的第一个参数已经是cls了
     def __init__(cls, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for n, name in enumerate(cls._fields_):
             setattr(cls, name, property(operator.itemgetter(n)))
 
+
 class StructTuple(tuple, metaclass=StructTupleMeta):
-    _fields_ = []
+    _fields_ = []   # 之所以需要是因为StructTupleMeta在__init__时需要
     def __new__(cls, *args):
         if len(args) != len(cls._fields_):
             raise ValueError('{} arguments required'.format(len(cls._fields_)))
         return super().__new__(cls,args)
+
 
 # Examples
 class Stock(StructTuple):
