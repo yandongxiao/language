95d6c7530962340f338c61b3f3a4778763ee0501
yandongxiao <yandongxiao@chinac.com>
2017-07-29 19:43:04 +0800

    在元类上做文章的两个地方：
    1. __new__(cls, clsname, parrent, mydict) 为类对象增添一些属性
    2. __prepare__返回一个自定义的字典类，对字典类实施一些特殊方法，限制
    class 中属性的创建

--- python-cookbook-master/src/9/capturing_class_attribute_definition_order/example2.py
+++ python-cookbook-master/src/9/capturing_class_attribute_definition_order/example2.py
@@ -18,8 +18,10 @@ class NoDupOrderedDict(OrderedDict):
             raise TypeError('{} already defined in {}'.format(name, self.clsname))
         super().__setitem__(name, value)
 
+
 class OrderedMeta(type):
     def __new__(cls, clsname, bases, clsdict):
+        print("can not be here")
         d = dict(clsdict)
         d['_order'] = [name for name in clsdict if name[0] != '_']
         return type.__new__(cls, clsname, bases, d)
@@ -37,3 +39,4 @@ class A(metaclass=OrderedMeta):
     def spam(self):
         pass
 
+print(A._order)
