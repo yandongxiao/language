b07bba45b468cf69c76a22ade500d7311e563ed5
yandongxiao <yandongxiao@chinac.com>
2017-07-30 20:32:04 +0800

    通过locals函数可以获取局部变量表，多个locals函数返回的是同一个表，
    表是动态构建的，即动态收集当前局部变量的值。
    exec()在局部环境下执行时，就是在locals表中进行的。python外部环境并没有更
    改

--- python-cookbook-master/src/9/executing_code_with_local_side_effects/example.py
+++ python-cookbook-master/src/9/executing_code_with_local_side_effects/example.py
@@ -1,14 +1,21 @@
+#! /usr/bin/env python3
+# encoding: utf-8
+
+# NOTE: exec在python3中大变样
+# 实际上对于 exec() 的正确使用是比较难的。大多数情况下当你要考虑使用 exec() 的时候
+# 应该采用更好的解决办法
+
 def test():
     a = 13
-    loc = locals()
+    loc = locals()      # 获取局部变量字典
     exec('b = a + 1')
     b = loc['b']
-    print(b)      # --> 14
+    assert b == 14
 
 def test1():
     x = 0
     exec('x += 1')
-    print(x)      # --> 0
+    assert x == 0
 
 def test2():
     x = 0
@@ -16,24 +23,25 @@ def test2():
     print('before:', loc)
     exec('x += 1')
     print('after:', loc)
-    print('x =', x)
+    assert x == 0
+    assert loc['x'] == 1
 
 def test3():
     x = 0
     loc = locals()
-    print(loc)
+    assert loc['x'] == 0
     exec('x += 1')
-    print(loc)
+    assert loc['x'] == 1
     locals()
-    print(loc)
- 
+    assert loc['x'] == 0
+
 def test4():
     a = 13
     loc = { 'a' : a }
     glb = { }
     exec('b = a + 1', glb, loc)
     b = loc['b']
-    print(b)
+    assert b == 14
 
 if __name__ == '__main__':
     print(':::: Running test()')
@@ -50,8 +58,3 @@ if __name__ == '__main__':
 
     print(':::: Running test4()')
     test4()
-
- 
-
- 
-
