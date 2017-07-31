e370eeecb8c4cf01063810e1fde8dc99c24f944b
yandongxiao <yandongxiao@chinac.com>
2017-07-31 14:21:08 +0800

    将一个模块分散在多个文件当中

--- python-cookbook-master/src/10/splitting_a_module_into_multiple_files/example.py
+++ python-cookbook-master/src/10/splitting_a_module_into_multiple_files/example.py
@@ -1,6 +1,26 @@
+# /usr/bin/env python
+# encoding: utf-8
+
+print "====method1===="
 import mymodule
 a = mymodule.A()
 a.spam()
-
 b = mymodule.B()
+b.bar()
+
+print "====method2===="
+from mymodule.a import A
+from mymodule.b import B
+a = A()
+a.spam()
+b = B()
+b.bar()
+A = 10
+A = 20
+
+print "====method3===="
+from mymodule import A, B
+a = A()
+a.spam()
+b = B()
 b.bar()
--- python-cookbook-master/src/10/splitting_a_module_into_multiple_files/mymodule/__init__.py
+++ python-cookbook-master/src/10/splitting_a_module_into_multiple_files/mymodule/__init__.py
@@ -1,5 +1,8 @@
-# __init__.py
+# /usr/bin/env python
+# encoding: utf-8
+
+# 将一个个具体的类引入进来
+# 考虑如何做延迟导入
 
 from .a import A
 from .b import B
-
