bf11b896034db368f16264b8a4b1cd8bac0cf27e
yandongxiao <yandongxiao@chinac.com>
2017-07-31 13:41:23 +0800

    模块中不存在变量变形这一说，下划线开头的变量，如果你硬是要访问也是可以的，不赞成

--- python-cookbook-master/src/10/a/c/__init__.py
+++ python-cookbook-master/src/10/a/c/__init__.py
@@ -5,5 +5,7 @@ __all__ = ["c"]
 
 c = 20
 cn = "helloworld"
-
+_cx = 100
+__cyy = 200
+__czz__ = 300
 print "package c"
--- python-cookbook-master/src/10/a/main.py
+++ python-cookbook-master/src/10/a/main.py
@@ -8,3 +8,6 @@ from a import c
 print "main"
 print dir()
 print c.cn
+print c._cx     # 私有变量也是可以访问到的
+print c.__cyy
+print c.__czz__
