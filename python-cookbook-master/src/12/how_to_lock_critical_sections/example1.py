62002e57502c3d7526b771ae254fac4a6039ca10
yandongxiao <yandongxiao@chinac.com>
2017-08-02 14:57:41 +0800

    锁与with语句是好搭档

--- python-cookbook-master/src/12/how_to_lock_critical_sections/example1.py
+++ python-cookbook-master/src/12/how_to_lock_critical_sections/example1.py
@@ -1,17 +1,26 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+# 使用一个锁对一个可变变量进行保护，同时将他们封装成一个类，那么类对象就是线程安全的
+# RLock是可重入的锁，即一个线程可以再次获取同一个锁
+# 信号量比锁更耗费资源，所以能使用锁的地方，不使用信号量
+# 信号量主要是用来控制并发数的
+
 import threading
 
 class SharedCounter:
     '''
     A counter object that can be shared by multiple threads.
     '''
+
     def __init__(self, initial_value = 0):
         self._value = initial_value
         self._value_lock = threading.Lock()
 
     def incr(self,delta=1):
         '''
-	Increment the counter with locking
-	'''
+	    Increment the counter with locking
+	    '''
         with self._value_lock:
              self._value += delta
 
@@ -40,6 +49,6 @@ if __name__ == '__main__':
     t1.join()
     t2.join()
     t3.join()
-    
+
     assert c._value == 0
     print('Looks good!')
