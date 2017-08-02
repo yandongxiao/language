f23271ad6626e5135160c42f98aebabe8febb977
yandongxiao <yandongxiao@chinac.com>
2017-08-02 14:01:55 +0800

    两个线程之间最有效的通信方式是Queue，当然我们也可以借助Condition、信号量 实现自己的Queue
    Queue是JoinableQueue的。即生产者调用join方法后，被阻塞，直到Queue被清空
    消费者调用对应的task_done。注意，task_done不一定会将生产者从阻塞中拯救出
    来，还有一个条件就是Queue为空

--- python-cookbook-master/src/12/how_to_communicate_between_threads/example1.py
+++ python-cookbook-master/src/12/how_to_communicate_between_threads/example1.py
@@ -1,43 +1,52 @@
-from queue import Queue
-from threading import Thread
-import time
+#! /usr/bin/env python
+# encoding: utf-8
 
+import time
+import Queue
+import threading
+
+ # 唯一标识了一个对象
 _sentinel = object()
+
 
 # A thread that produces data
 def producer(out_q):
-    n = 10
+    n = 3
     while n > 0:
         # Produce some data
         out_q.put(n)
-        time.sleep(2)
+        out_q.put(n)
+        print('put:', n)
+        print('put:', n)
+        out_q.join()    # 只有当队列中元素清空以后，才会被返回
         n -= 1
-
-
     # Put the sentinel on the queue to indicate completion
     out_q.put(_sentinel)
+    print('Producer shutting down')
+
 
 # A thread that consumes data
 def consumer(in_q):
     while True:
         # Get some data
         data = in_q.get()
-
         # Check for termination
         if data is _sentinel:
+            # 消费者在读到这个特殊值之后立即又把它放回到队列中，将之传递下去。
+            # 这样，所有监听这个队列的消费者线程就可以全部关闭了
             in_q.put(_sentinel)
             break
-
         # Process the data
+        time.sleep(1)
         print('Got:', data)
+        in_q.task_done()
     print('Consumer shutting down')
 
 if __name__ == '__main__':
-    q = Queue()
-    t1 = Thread(target=consumer, args=(q,))
-    t2 = Thread(target=producer, args=(q,))
+    q = Queue.Queue()
+    t1 = threading.Thread(target=consumer, args=(q,))
+    t2 = threading.Thread(target=producer, args=(q,))
     t1.start()
     t2.start()
     t1.join()
     t2.join()
-
--- python-cookbook-master/src/12/how_to_communicate_between_threads/example2.py
+++ python-cookbook-master/src/12/how_to_communicate_between_threads/example2.py
@@ -1,23 +1,30 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
 import heapq
 import threading
 import time
 
-class PriorityQueue:
+# 原则1：两个线程共享一个对象，queue也是这样做的
+class PriorityQueue:        # 实现了一个线程安全的优先级队列
     def __init__(self):
         self._queue = []
         self._count = 0
         self._cv = threading.Condition()
+
     def put(self, item, priority):
-        with self._cv:
+        with self._cv:  # 获取锁
+            # 向heaq中添加元素
             heapq.heappush(self._queue, (-priority, self._count, item))
-            self._count += 1
+            self._count += 1    # 保证队列元素的唯一性
             self._cv.notify()
 
     def get(self):
-        with self._cv:
+        with self._cv:  # 获取锁
             while len(self._queue) == 0:
                 self._cv.wait()
             return heapq.heappop(self._queue)[-1]
+
 
 def producer(q):
     print('Producing items')
@@ -25,7 +32,9 @@ def producer(q):
     q.put('A', 15)
     q.put('B', 10)
     q.put('D', 0)
-    q.put(None, -100)
+    q.put(None, -100)   # 结尾元素
+    print('Producer done')
+
 
 def consumer(q):
     time.sleep(5)
@@ -37,6 +46,7 @@ def consumer(q):
         print('Got:', item)
     print('Consumer done')
 
+
 if __name__ == '__main__':
     q = PriorityQueue()
     t1 = threading.Thread(target=producer, args=(q,))
@@ -45,5 +55,3 @@ if __name__ == '__main__':
     t2.start()
     t1.join()
     t2.join()
-
-        
