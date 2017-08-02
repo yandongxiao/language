a5afb3e406652ec0b93a706759cb81b8beae7991
yandongxiao <yandongxiao@chinac.com>
2017-08-02 13:14:32 +0800

    wait执行后进入睡眠状态，被唤醒后；如果有多个线程被唤醒，那么他们要抢锁。
    未抢到锁的线程也没有再次进行睡眠状态，只要锁一旦被释放，就立刻去抢锁

--- python-cookbook-master/src/12/how_to_determine_if_a_thread_has_started/example2.py
+++ python-cookbook-master/src/12/how_to_determine_if_a_thread_has_started/example2.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
 import threading
 import time
 
@@ -5,6 +8,7 @@ class PeriodicTimer:
     def __init__(self, interval):
         self._interval = interval
         self._flag = 0
+        # 与Event对象的创建方法是一样的
         self._cv = threading.Condition()
 
     def start(self):
@@ -17,6 +21,12 @@ class PeriodicTimer:
         Run the timer and notify waiting threads after each interval
         '''
         while True:
+            # since notify() does not release the lock, its caller should.
+            # Condition 支持上下文管理协议，目的是为了隐藏释放底层锁的问题，每次while循环都会释放该锁
+            # 为什么Event就是一次性的，而Condition就可以重复利用呢？ 就是因为Condition的底层还有一个锁
+            # 为什么notify_all方法会导致两个线程并行返回呢？不是说，Once awakened or timed out, it re-acquires the lock and returns.
+            # 按照上面的道理，虽然是notify_all，但是最终也只能有一个线程能获取该锁。
+            # 没错，只有一个线程会获取该锁，但是另外一个线程从睡眠状态醒过来，只等着前面那个线程释放锁，就可以工作了。它不需要再次被唤醒
             time.sleep(self._interval)
             with self._cv:
                  self._flag ^= 1
@@ -26,13 +36,16 @@ class PeriodicTimer:
         '''
         Wait for the next tick of the timer
         '''
-        with self._cv:
+        with self._cv:      # 释放锁的速度很快的
+            # 注意：为什么使用while循环？
+            # last_flag == self._flag 就是所谓的Condition了，如果条件不满足，逻辑上当然不应该醒来
             last_flag = self._flag
             while last_flag == self._flag:
-                self._cv.wait()
+                self._cv.wait()     # 会主动将锁释放, 并进入睡眠状态
+
 
 # Example use of the timer
-ptimer = PeriodicTimer(5)
+ptimer = PeriodicTimer(1)
 ptimer.start()
 
 # Two threads that synchronize on the timer
@@ -49,5 +62,5 @@ def countup(last):
         print("Counting", n)
         n += 1
 
-threading.Thread(target=countdown, args=(10,)).start()
+threading.Thread(target=countdown, args=(5,)).start()
 threading.Thread(target=countup, args=(5,)).start()
