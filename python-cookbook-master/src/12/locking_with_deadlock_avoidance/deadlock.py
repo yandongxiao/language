b24cf4e35027045ef576fa6e9f9d7c409520a0f3
yandongxiao <yandongxiao@chinac.com>
2017-08-02 15:17:26 +0800

    防止死锁的两个原则：
    1. 一个线程只使用一个锁
    2. 线程使用多个锁时，按照预定的顺序获取锁

--- python-cookbook-master/src/12/locking_with_deadlock_avoidance/deadlock.py
+++ python-cookbook-master/src/12/locking_with_deadlock_avoidance/deadlock.py
@@ -1,16 +1,22 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
 import threading
 from contextlib import contextmanager
 
 # Thread-local state to stored information on locks already acquired
+# 当前线程的堆栈
 _local = threading.local()
 
+# contextmanager 很轻松的实现一个上线文管理协议
 @contextmanager
 def acquire(*locks):
     # Sort locks by object identifier
-    locks = sorted(locks, key=lambda x: id(x))   
+    locks = sorted(locks, key=lambda x: id(x))
 
+    # 中间这一段是防御性编程
     # Make sure lock order of previously acquired locks is not violated
-    acquired = getattr(_local, 'acquired',[])
+    acquired = getattr(_local, 'acquired', [])
     if acquired and max(id(lock) for lock in acquired) >= id(locks[0]):
         raise RuntimeError('Lock Order Violation')
 
