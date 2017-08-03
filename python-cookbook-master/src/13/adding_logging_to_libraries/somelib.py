3616000067b9bb12a7b10747894f939fbc30b99f
yandongxiao <yandongxiao@chinac.com>
2017-08-03 11:23:53 +0800

    logging.getLogger(__filename__)

--- python-cookbook-master/src/13/adding_logging_to_libraries/somelib.py
+++ python-cookbook-master/src/13/adding_logging_to_libraries/somelib.py
@@ -1,8 +1,14 @@
-# somelib.py
+#! /usr/bin/env python
+# encoding: utf-8
 
 import logging
+
+# 注意logging 和 logger 的区别
+# logging是一个模块，该模块下的setBasicConfig方法，对整体日志系统进行配置
+# logger是一个对象，一般一个模块对应一个logger。
+# NOTE: logger可以定制自己的日志配置
 log = logging.getLogger(__name__)
-log.addHandler(logging.NullHandler())
+log.addHandler(logging.NullHandler())   # 如果日至系统配置不存在，则忽略打印信息
 
 # Example function (for testing)
 def func():
--- python-cookbook-master/src/13/finding_files/modified_within.py
+++ python-cookbook-master/src/13/finding_files/modified_within.py
@@ -12,16 +12,12 @@ def modified_within(top, seconds):
                 mtime = os.path.getmtime(fullpath)
                 if mtime > (now - seconds):
                     print(fullpath)
-            
+
 
 if __name__ == '__main__':
     import sys
     if len(sys.argv) != 3:
         print('Usage: {} dir seconds'.format(sys.argv[0]))
         raise SystemExit(1)
-    
+
     modified_within(sys.argv[1], float(sys.argv[2]))
-
-    
-
-    
