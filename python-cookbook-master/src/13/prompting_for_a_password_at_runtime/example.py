2e91da4c0895f434984308c174095682e3bb32f5
yandongxiao <yandongxiao@chinac.com>
2017-08-03 10:24:21 +0800

    getpass模块

--- python-cookbook-master/src/13/prompting_for_a_password_at_runtime/example.py
+++ python-cookbook-master/src/13/prompting_for_a_password_at_runtime/example.py
@@ -1,7 +1,8 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
 import getpass
-
-user = getpass.getuser()
+user = getpass.getuser()    # 返回的是系统的用户名称
 passwd = getpass.getpass()
-
 print('User:', user)
 print('Passwd:', passwd)
