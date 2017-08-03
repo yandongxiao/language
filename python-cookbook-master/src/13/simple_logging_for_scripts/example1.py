7ac1d00fed3011137b34fe0e41fcb3b446b37cd9
yandongxiao <yandongxiao@chinac.com>
2017-08-03 10:54:58 +0800

    import logging logging.baseConfig filename level format

--- python-cookbook-master/src/13/simple_logging_for_scripts/example1.py
+++ python-cookbook-master/src/13/simple_logging_for_scripts/example1.py
@@ -1,10 +1,13 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
 import logging
 
 def main():
     # Configure the logging system
     logging.basicConfig(
-        filename='app.log',
-        level=logging.ERROR
+        filename='app.log',     # 日志的名称
+        level=logging.ERROR     # 日志界别
     )
 
     # Variables (to make the calls that follow work)
@@ -14,8 +17,9 @@ def main():
     mode = 'r'
 
     # Example logging calls (insert into your program)
+    # logging本身也是支持格式的字符串的
     logging.critical('Host %s unknown', hostname)
-    logging.error("Couldn't find %r", item)
+    logging.error("Couldn't find %s", item)
     logging.warning('Feature is deprecated')
     logging.info('Opening file %r, mode=%r', filename, mode)
     logging.debug('Got here')
