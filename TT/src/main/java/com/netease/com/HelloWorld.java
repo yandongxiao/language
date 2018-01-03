7884d1c8c72112c56880c503ff0871b2c7964797
yandongxiao <dxyan06@gmail.com>
2018-01-03 14:30:50 +0800

    update

---
+++ TT/src/main/java/com/netease/com/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.netease.com;
+
+/**
+ * TODO: Write the file descriotion here
+ *
+ * @author yandongxiao
+ * @create 2017/10/21
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+    }
+}
---
+++ thinking-in-java/initialization/Value.java
@@ -0,0 +1,10 @@
+public class Value {
+    String str = "hello";
+    Value() {
+        System.out.println(str);
+    }
+
+    public static void main (String[] str) {
+        Value v = new Value();
+    }
+}
