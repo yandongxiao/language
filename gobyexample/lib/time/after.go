1536b250d42e1a1967e1f89840f30002f842974b
yandongxiao <dxyan06@gmail.com>
2018-06-29 22:23:39 +0800

    update

--- gobyexample/lib/time/after.go
+++ gobyexample/lib/time/after.go
@@ -14,6 +14,14 @@ func basic() {
 	fmt.Println(<-ch) // 返回值不是100ms以后的值，并非是1秒以后的值
 }
 
+func useTimer() {
+	timer := time.NewTimer(100 * time.Millisecond)
+	fmt.Println(time.Now())
+	time.Sleep(time.Second)
+	fmt.Println(<-timer.C)
+}
+
 func main() {
 	basic()
+	useTimer()
 }
