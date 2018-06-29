4dc8a16f3bcb0d24a92dcb1f43434feea474d96f
yandongxiao <dxyan06@gmail.com>
2018-06-29 19:10:46 +0800

    update

---
+++ gobyexample/lib/time/after.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func basic() {
+	// It is equivalent to NewTimer(d).C
+	// If efficiency is a concern, use NewTimer instead and call Timer.Stop if the timer is no longer needed.
+	ch := time.After(100 * time.Millisecond)
+	fmt.Println(time.Now())
+	time.Sleep(time.Second)
+	fmt.Println(<-ch) // 返回值不是100ms以后的值，并非是1秒以后的值
+}
+
+func main() {
+	basic()
+}
--- gobyexample/lib/time/time.go
+++ gobyexample/lib/time/time.go
@@ -1,3 +1,6 @@
+// time
+// Note that the Go == operator compares not just the time instant but also the Location.
+// 所以：time.Time = 数字 + 时区。
 package main
 
 import (
@@ -5,23 +8,52 @@ import (
 	"time"
 )
 
-var week time.Duration
+// NOTE: Location仅用于时区转化，而不对time内部的数据产生影响
+func loadLocation() {
+	println("loadLocation")
+	// 等同于"UTC", 世界标准时间
+	now := time.Now()
+	local1, err1 := time.LoadLocation("") // 等同于传递"UTC"
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+
+	// CST: 北京时间
+	// 在国外获取中国北京时间，要用"PRC"
+	local2, err2 := time.LoadLocation("Local")
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+
+	// 美国洛杉矶时间
+	local3, err3 := time.LoadLocation("America/Los_Angeles")
+	if err3 != nil {
+		fmt.Println(err3)
+	}
+
+	fmt.Println(now) // Now的时区信息是Local
+	fmt.Println(now.In(local1))
+	fmt.Println(now.In(local2))
+	fmt.Println(now.In(local3))
+}
+
+// Format
+func format() {
+	println("format")
+	t := time.Now().UTC() // 此时返回了一个新的time.Time值，它是标准世界时间
+	fmt.Println(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+	// -0700 对应的输出是±hhmm，表示与世界标准时间的差值.
+	// MST是比较有魔力的一个值，他会被转换为UTC（世界标准时间）
+	fmt.Println(t.Format("Mon Jan 2 15:04:05 -0700 MST 2006")) // GMT和UTC是两种不同的时间记录方式，都是世界标准时间
+	fmt.Println(t.Format("Mon Jan 2 15:04:05 GMT 2006"))       // GMT是一个普通的字符串
+
+	now := time.Now() // 返回的是Local时区的时间
+	// MST是比较有魔力的一个值，他会被转换为CST（北京时间）
+	fmt.Println(now.Format("Mon Jan 2 15:04:05 -0700 MST 2006")) // GMT和UTC是两种不同的时间记录方式，都是世界标准时间
+	fmt.Println(now.Format("Mon Jan 2 15:04:05 GMT 2006"))
+}
 
 func main() {
-	t := time.Now()
-	fmt.Println(t)     // Wed Dec 21 09:52:14 +0100 RST 2011
-	fmt.Printf("%02d.%02d.%4d\n", t.Day(), t.Month(), t.Year()) // 21.12.2011
-	t = time.Now().UTC()
-	fmt.Println(t)          // Wed Dec 21 08:52:14 +0000 UTC 2011
-	fmt.Println(time.Now()) // Wed Dec 21 09:52:14 +0100 RST 2011
-	// calculating times:
-	week = 60 * 60 * 24 * 7 * 1e9  // must be in nanosec
-	week_from_now := t.Add(week)
-	fmt.Println(week_from_now)      // Wed Dec 28 08:52:14 +0000 UTC 2011
-	// formatting times:
-	fmt.Println(t.Format(time.RFC822))     // 21 Dec 11 0852 UTC
-	fmt.Println(t.Format(time.ANSIC))      // Wed Dec 21 08:56:34 2011
-	fmt.Println(t.Format("02 Jan 2006 15:04")) // 21 Dec 2011 08:52
-	s := t.Format("20060102")
-	fmt.Println(t, "=>", s) // Wed Dec 21 08:52:14 +0000 UTC 2011 => 20111221
+	loadLocation()
+	format()
 }
