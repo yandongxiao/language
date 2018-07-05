711f43df2ad87138057021dd62b9ad5c19bab5e1
yandongxiao <dxyan06@gmail.com>
2018-07-05 16:39:52 +0800

    update

--- code/leetcode/delete_duplicate.go
+++ code/leetcode/delete_duplicate.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Definition for singly-linked list.
 type ListNode struct {
 	Val  int
@@ -8,12 +10,59 @@ type ListNode struct {
 
 func deleteDuplicates(head *ListNode) *ListNode {
 	var preNode *ListNode
+	var lastPreNode *ListNode
+	counter := 0
 	for node := head; node != nil; node = node.Next {
-		if preNode == nil || preNode.Val != node.Val {
+		if preNode == nil || preNode.Val == node.Val {
 			preNode = node
-		} else {
-			preNode.Next = node.Next
+			counter++
+		} else if counter > 1 {
+			preNode = node
+			counter = 1
+		} else if counter == 1 {
+			if lastPreNode == nil {
+				lastPreNode = preNode
+				head = lastPreNode
+			} else {
+				lastPreNode.Next = preNode
+				lastPreNode = preNode
+			}
+			preNode = node
 		}
 	}
+	println(counter)
+	if counter == 1 {
+		if lastPreNode == nil {
+			lastPreNode = preNode
+			head = lastPreNode
+		} else {
+			lastPreNode.Next = preNode
+			lastPreNode = preNode
+			lastPreNode.Next = nil
+		}
+		return head
+	}
+
+	if lastPreNode == nil {
+		return nil
+	}
+
+	lastPreNode.Next = nil
 	return head
+}
+
+func main() {
+	//l4 := &ListNode{1, nil}
+	l3 := &ListNode{2, nil}
+	l2 := &ListNode{2, l3}
+	l1 := &ListNode{1, l2}
+	head := deleteDuplicates(l1)
+	for {
+		if head != nil {
+			fmt.Println(head.Val)
+			head = head.Next
+			continue
+		}
+		break
+	}
 }
